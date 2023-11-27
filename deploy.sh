69ee5b8c59d2bf58df6fee36c32aff4078276371
root <root@ip-172-31-4-216.ap-south-1.compute.internal>
2023-11-27 14:03:15 +0000

    hello

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+docker login -u bhuvaneswarrr -p dckr_pat_1BcOwgl4e06z167bGY0uDK7ECbY
+if [[ "$GIT_BRANCH" == "master" ]]; then
+./build.sh
+docker tag caps bhuvaneswarrr/prod
+docker push bhuvaneswarrr/prod
+elif [[ "$GIT_BRANCH" == "dev" ]]; then
+./build.sh
+docker tag caps bhuvaneswarrr/dev
+docker push bhuvaneswarrr/dev
+else 
+  echo 'deployment failure have look"
+fi
