0baba57891fa40b07d8d0f85499825994f18f25e
Bhuvaneswarrr <144918865+Bhuvaneswarrr@users.noreply.github.com>
2023-11-28 17:49:43 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 docker login -u bhuvaneswarrr -p dckr_pat_1BcOwgl4e06z167bGY0uDK7ECbY
-if [[ "$GIT_BRANCH" == "master" ]]; then
+if [[ $GIT_BRANCH == "master" ]]; then
 ./build.sh
 docker tag caps bhuvaneswarrr/prod
 docker push bhuvaneswarrr/prod
-elif [[ "$GIT_BRANCH" == "dev" ]]; then
+elif [[ $GIT_BRANCH == "dev" ]]; then
 ./build.sh
 docker tag caps bhuvaneswarrr/dev
 docker push bhuvaneswarrr/dev
